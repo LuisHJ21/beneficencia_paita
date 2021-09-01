7575edc4e7e7c0bd7f9eaa65ea46cf21b570fafe
Luis <cuentagamer211197@gmail.com>
2021-09-01 00:08:20 -0500

    paginacion agregada 2

--- app/Views/pagers/default_full.php
+++ app/Views/pagers/default_full.php
@@ -1,7 +1,7 @@
 <?php $pager->setSurroundCount(2) ?>
 
 <nav aria-label="Page navigation">
-    <ul class="pagination">
+    <ul class="pagination pagination-lg">
     <?php if ($pager->hasPrevious()) : ?>
        
         <li class="page-item">
